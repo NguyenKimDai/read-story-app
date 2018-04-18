1df4901560d0503cc007a3897d678d2320395ef8
Hai Dang <danghuyen2504@gmai.com>
2018-04-18 09:13:09 +0700

    UI user

--- src/components/User.js
+++ src/components/User.js
@@ -7,7 +7,7 @@ import {Container,
         Icon,
         Button
 } from 'native-base';
-import { ImageBackground, ScrollView, TouchableOpacity } from 'react-native';
+import { ImageBackground, ScrollView, TouchableHighlight, Alert } from 'react-native';
 
 export default class User extends Component {
     static navigationOptions = ({navigation}) => ({
@@ -29,29 +29,25 @@ export default class User extends Component {
                             <Thumbnail large source = {require('../images/avatar.png')}/>
                         </View>
 
-                        <TouchableOpacity style = {{alignItems: 'center', marginTop: 15}}>
-                            <Text style = {{padding: 10, borderWidth: 2, borderColor: '#19c49c', borderRadius: 50}}>Đăng nhập</Text>
-                        </TouchableOpacity>
+                        <TouchableHighlight style = {{alignItems: 'center', marginTop: 15}}
+                                            underlayColor = '#19c49c'
+                                            onPress = {() => {Alert.alert('Touched')}}>
+                            <Text style = {{padding: 10, borderWidth: 1, borderColor: '#19c49c', borderRadius: 50}}>Đăng nhập</Text>
+                        </TouchableHighlight>
 
                         <View style = {{marginTop: 20, marginHorizontal: 15}}>
-                            <TouchableOpacity>
-                            <Item style = {{paddingVertical: 10}}>
+                            <Item style = {{paddingVertical: 20, alignItems: 'center'}}>
                                 <Icon name = 'ios-create-outline'/>
                                 <Text>Trở thành tác giả</Text>
                             </Item>
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                            <Item style = {{paddingVertical: 10}}>
+                            <Item style = {{paddingVertical: 20, alignItems: 'center'}}>
                                 <Icon name = 'ios-chatbubbles-outline'/>
                                 <Text>Phản hồi</Text>
                             </Item>
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                            <Item style = {{paddingVertical: 10}}>
+                            <Item style = {{paddingVertical: 20, alignItems: 'center'}}>
                                 <Icon name = 'ios-settings-outline'/>
                                 <Text>Thiết lập</Text>
                             </Item>
-                            </TouchableOpacity>
                         </View>
                     </ScrollView>
                 </View>
